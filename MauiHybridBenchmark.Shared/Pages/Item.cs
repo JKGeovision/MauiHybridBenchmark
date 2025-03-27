5702f50135347ca213aab5a8b77ed65292a81a58
JKGeovision <jk@geovision.de>
2025-03-27 17:26:03 +0100

    Benchmark added

---
+++ MauiHybridBenchmark.Shared/Pages/Item.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiHybridBenchmark.Shared.Pages;
+
+public class Item
+{
+    public DateTime Time { get; set; }
+
+    public short Type { get; set; }
+
+    public short Status => (short)(Type & 0x0ff);
+}
